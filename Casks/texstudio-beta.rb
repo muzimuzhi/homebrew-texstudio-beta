c1841ea365239fc532c31f6ffa7d659a0c5d7311
muzimuzhi <muzimuzhi@gmail.com>
2020-07-28 19:15:42 +0800

    fix RuboCop style
    see Homebrew/brew#7867

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -1,20 +1,20 @@
-cask 'texstudio-beta' do
-  version '3.0.0beta3'
-  sha256 'd8873e75e8e618f233b0a6658311c9420bfbe1dd6c474a4d240658a92fff318a'
+cask "texstudio-beta" do
+  version "3.0.0beta3"
+  sha256 "d8873e75e8e618f233b0a6658311c9420bfbe1dd6c474a4d240658a92fff318a"
 
   # github.com/texstudio-org/texstudio/ was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg"
-  appcast 'https://github.com/texstudio-org/texstudio/releases.atom'
-  name 'TeXstudio'
-  homepage 'https://texstudio.org/'
+  appcast "https://github.com/texstudio-org/texstudio/releases.atom"
+  name "TeXstudio"
+  homepage "https://texstudio.org/"
 
-  conflicts_with cask: 'texstudio'
+  conflicts_with cask: "texstudio"
 
-  app 'texstudio.app'
+  app "texstudio.app"
 
   zap trash: [
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/texstudio.sfl*',
-               '~/Library/Preferences/texstudio.plist',
-               '~/Library/Saved Application State/texstudio.savedState',
-             ]
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/texstudio.sfl*",
+    "~/Library/Preferences/texstudio.plist",
+    "~/Library/Saved Application State/texstudio.savedState",
+  ]
 end
