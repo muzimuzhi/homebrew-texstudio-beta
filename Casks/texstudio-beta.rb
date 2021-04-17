256263f685319001b2ec0a77bc1b32df226fc371
muzimuzhi <muzimuzhi@gmail.com>
2021-04-18 00:42:14 +0800

    Update texstudio-beta from 3.1.2beta2 to 3.1.2beta3

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -1,6 +1,6 @@
 cask "texstudio-beta" do
-  version "3.1.2beta2"
-  sha256 "f9b2197dbcdbaef73a4e66dfdb5960b7bd08a8ed2c5534a3ace6a5ae847ab5f5"
+  version "3.1.2beta3"
+  sha256 "15c488c63bc0ac694932c84ef375b42f3d0d98b44f22a22d023bae1f0e1024db"
 
   # github.com/texstudio-org/texstudio/ was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg",
