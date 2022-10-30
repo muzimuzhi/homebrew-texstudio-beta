6685e0431cfb82747fb2a96d9dc61317969aa5c7
muzimuzhi <muzimuzhi@gmail.com>
2022-10-30 17:56:10 +0800

    Update texstudio-beta from 4.4.0beta1 to 4.4.0beta2

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -1,6 +1,6 @@
 cask "texstudio-beta" do
-  version "4.4.0beta1"
-  sha256 "410652b81958965c4329bc028c659f64c0e368c2a900ecc508daaeffe2e947b2"
+  version "4.4.0beta2"
+  sha256 "2d8162caabad5859cd0ddec166258c14c12eb0a98cf33d604b27d77cc1416707"
 
   # github.com/texstudio-org/texstudio/ was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg",
