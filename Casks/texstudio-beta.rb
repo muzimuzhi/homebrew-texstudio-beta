8fabc8857c9e120e835aaef4cbcf37e5e9747451
muzimuzhi <muzimuzhi@gmail.com>
2023-01-21 21:18:37 +0800

    texstudio-beta 4.5.1beta5
    Update texstudio-beta from 4.5.1beta4 to 4.5.1beta5
    
    Closes #13.
    
    Signed-off-by: muzimuzhi <6376638+muzimuzhi@users.noreply.github.com>

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -1,6 +1,6 @@
 cask "texstudio-beta" do
-  version "4.5.1beta4"
-  sha256 "24c99d5c76afa643dbefc66fd3bc23bf2133a29d628836cd41a8060431e1a72f"
+  version "4.5.1beta5"
+  sha256 "fd1730e5a48ef81a540a99c86c67e87bce73e1fd7d93f14533a3850f0e0cc6ef"
 
   # github.com/texstudio-org/texstudio/ was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg",
