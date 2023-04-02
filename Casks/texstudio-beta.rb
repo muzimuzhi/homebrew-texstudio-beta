cf59bce7a7c2b60ca269f5f88ee69c3ac3e97db1
Yukai Chou <muzimuzhi@gmail.com>
2023-04-02 21:19:31 +0800

    texstudio: add livecheck
    Emulates https://github.com/Homebrew/homebrew-cask/commit/70e1bb9a2b83ea6478b144a447744c5dbdb1e03d

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -2,10 +2,8 @@ cask "texstudio-beta" do
   version "4.5.1rc1"
   sha256 "d548c291bca4a880d75e02debb1be9fc655c54d1c3c900ab557f2734824bf137"
 
-  # github.com/texstudio-org/texstudio/ was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg",
       verified: "https://github.com/texstudio-org/texstudio/"
-  appcast "https://github.com/texstudio-org/texstudio/releases.atom"
   name "TeXstudio"
   desc "Fully featured LaTeX editor, beta version"
   homepage "https://texstudio.org/"
