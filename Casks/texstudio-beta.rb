f74a536ac8f402d9c022810e1c7e1cff1e6bc72a
Yukai Chou <muzimuzhi@gmail.com>
2023-04-08 04:31:45 +0800

    texstudio-beta 4.5.2rc1

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -1,6 +1,6 @@
 cask "texstudio-beta" do
-  version "4.5.2beta4"
-  sha256 "659dbaede6ecff96d66b203ed28d2267e68c84e4a441dc0e727cd3549b1ddeb1"
+  version "4.5.2rc1"
+  sha256 "7c9521b1fa0b0ee5854cf90f5fb5e4cb6ed162b638531480003da99c6bca353e"
 
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg",
       verified: "github.com/texstudio-org/texstudio/"
