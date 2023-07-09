11bf2fa93f590f846471713c71606b8795f7f8c9
Yukai Chou <muzimuzhi@gmail.com>
2023-07-09 21:00:20 +0800

    texstudio-beta 4.6.0beta2
    Closes #25.
    
    Signed-off-by: Yukai Chou <6376638+muzimuzhi@users.noreply.github.com>

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -1,6 +1,6 @@
 cask "texstudio-beta" do
-  version "4.6.0beta1"
-  sha256 "ec43a09cab520c1db64b3f55542f2fe62dbeece3c845afc8faa6042d19f6196f"
+  version "4.6.0beta2"
+  sha256 "3d4a23ebbdd6fd0b2fbe34e79189ba59917714b6311c2ff82810038a78da8147"
 
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg",
       verified: "github.com/texstudio-org/texstudio/"
