48f5c7480055dcccbee48cc4b04608306fdadd82
Yukai Chou <muzimuzhi@gmail.com>
2023-09-27 08:05:24 +0800

    texstudio-beta: modify `:github_releases` block to accept pre-releases
    learnt from https://github.com/Homebrew/homebrew-cask-versions/pull/14970

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -9,8 +9,24 @@ cask "texstudio-beta" do
   homepage "https://texstudio.org/"
 
   livecheck do
+    # based on https://docs.brew.sh/Brew-Livecheck#githubreleases-strategy-block
     url :url
-    strategy :github_releases
+    regex(/^v?(\d+(?:\.\d+)+(?:(?:alpha|beta|rc)\d+)?)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+
+        match = release["tag_name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+    # :page_match can just list pre-releases
+    # learnt from https://github.com/Homebrew/homebrew-cask-versions/blob/948fe36253038658716087daac8c4b1f0ae0f7c3/Casks/utm-beta.rb#L11-L15
+    #   url "https://github.com/texstudio-org/texstudio/releases?q=prerelease%3Atrue&expanded=true"
+    #   regex(%r{href=["']?[^"' >]*?/tag/v?(\d+(?:\.\d+)+[^"' >]*)["' >]}i)
+    #   strategy :page_match
   end
 
   conflicts_with cask: "texstudio"
